c49740824404b27828c937c5271f4ef41cf48a8f
cole11smith33 <cole11smith33@gmail.com>
2025-03-01 13:53:22 -0500

    initialized class in explorer

--- src/main/java/ca/mcmaster/se2aa4/island/team09/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team09/Explorer.java
@@ -1,17 +1,19 @@
 package ca.mcmaster.se2aa4.island.team09;
 
 import java.io.StringReader;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import eu.ace_design.island.bot.IExplorerRaid;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+
+import eu.ace_design.island.bot.IExplorerRaid;
 
 public class Explorer implements IExplorerRaid {
 
     private final Logger logger = LogManager.getLogger();
     private DroneState drone;
+    private Radar radar;
     // TEMPORARY TESTING VARIABLES
     private int moveForward = 0;
     private int turnCount = 0;
@@ -25,6 +27,10 @@ public class Explorer implements IExplorerRaid {
         String startingDirection = info.getString("heading").toUpperCase();
         Integer batteryCapacity = info.getInt("budget");
         drone = new DroneState(startingDirection, new Battery(batteryCapacity));
+
+        radar = new Radar();
+        logger.info("Created instance of radar on drone");
+        
         logger.info("The drone is facing {}", drone.getDirection());
         logger.info("Battery level is {}", drone.getBatteryLevel());
     }
@@ -33,6 +39,9 @@ public class Explorer implements IExplorerRaid {
     @Override
     public String takeDecision() {
         JSONObject decision;
+
+        radar.check();
+
         // if drone is moving, complete right turn (KEEP THIS)
         if (drone.isDroneMoving()) {
             return drone.getDecision().toString();
--- src/main/java/ca/mcmaster/se2aa4/island/team09/Radar.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team09/Radar.java
@@ -1,8 +1,11 @@
 package ca.mcmaster.se2aa4.island.team09;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
-
 public class Radar {
+
+    private final Logger logger = LogManager.getLogger();
 
     public Radar() {
     }
@@ -16,11 +19,40 @@ public class Radar {
         JSONObject decision = new JSONObject();
         decision.put("action", "echo");
         decision.put("parameters", new JSONObject().put("direction", direction.toString()));
+        
         return decision;
     }
 
-    private Direction checkSurrondings() {
+    public void check(){
+        logger.info("DIRECTION CHECK");
+    }
+
+    //use getEcho class to find forward
+
+    //use getEcho class to find right
+
+    //use getEcho class to find left
+
+    //need a way to check the echo status (must incorporate echo status class)
+
+
+
+    private Direction checkSurrondingsNorth() {
         // should check boundaires for other directions to determine which way the drone should turn
         return Direction.N;  // placeholder
     }
+
+    private Direction checkSurrondingsEast() {
+        // should check boundaires for other directions to determine which way the drone should turn
+        return Direction.E;  // placeholder
+    }
+
+    private Direction checkSurrondingsWest() {
+        // should check boundaires for other directions to determine which way the drone should turn
+        return Direction.W;  // placeholder
+    }
+
+
+
+
 }
