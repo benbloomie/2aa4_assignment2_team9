b33d414d2cdd97043d9ef12f147ed92320fc1d52
benbloomie <bloomfib@mcmaster.ca>
2025-03-20 17:47:41 -0400

    refactored IslandLocater --> now properly updates coordinate after each action. encapsulates logic better

--- src/main/java/ca/mcmaster/se2aa4/island/team09/IslandLocater.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team09/IslandLocater.java
@@ -1,34 +1,61 @@
 package ca.mcmaster.se2aa4.island.team09;
 
 public class IslandLocater extends SearchPhase {
-    private int movesMade;
+    private int movesMadeToMiddle;
+    private int movesMadeToIsland;
+    private int rangeToGround;
 
     public IslandLocater(DroneState drone, CommandCenter commandCenter, ResponseCenter responseCenter, Island island) {
         super(drone, commandCenter, responseCenter, island);
-        this.movesMade = 0;
+        this.movesMadeToMiddle = 0;
+        this.movesMadeToIsland = 0;
     }
 
     @Override
     public void executeStep() {
         // moves drone forward to middle position
-        if (movesMade < island.getX()/2 - 1) {
-            drone.moveForward(commandCenter);
-            movesMade++;
+        if (movesMadeToMiddle < island.getX()/2 - 1) {
+            moveToMiddle();
+            return;
         }
         // turns drone when it reaches the middle of the map
-        else if (movesMade == island.getX()/2 - 1) {
-            drone.turnDrone(Direction.S, commandCenter);
-            drone.frontEcho(commandCenter);
-            movesMade++;
+        else if (movesMadeToMiddle == island.getX()/2 - 1) {
+            faceIsland();
+            return;
         }
+
+        setRangeToGround();
         // moves drone forward until it reaches GROUND 
-        else {
-            int movesToIsland = responseCenter.getRadarStatus().getRange();
-            for (int i = 0; i < movesToIsland; i++) {
-                drone.moveForward(commandCenter);
-            }
-            drone.turnDrone(Direction.E, commandCenter);
-            phaseCompleted = true;
+        if (movesMadeToIsland < rangeToGround) {
+            moveToIsland();
+            return;
+        }
+        // phase is completed
+        drone.turnDrone(Direction.E, commandCenter);
+        phaseCompleted = true;
+    }
+
+    private void moveToMiddle() {
+        drone.moveForward(commandCenter);
+        movesMadeToMiddle++;
+    }
+
+    private void faceIsland() {
+        // updates the drone to face the island; scans to get distance to island
+        drone.turnDrone(Direction.S, commandCenter);
+        drone.frontEcho(commandCenter);
+        movesMadeToMiddle++;
+    }
+
+    private void moveToIsland() {
+        drone.moveForward(commandCenter);
+        movesMadeToIsland++;
+    }
+
+    private void setRangeToGround() {
+        // now only makes one assignment to the range to GROUND
+        if (rangeToGround == 0) {
+            this.rangeToGround = responseCenter.getRadarStatus().getRange(); 
         }
     }
 }
