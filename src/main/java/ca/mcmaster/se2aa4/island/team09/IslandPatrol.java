57a376339d193d6f2795d6e3d26ff13f081d2ecb
cole11smith33 <cole11smith33@gmail.com>
2025-03-21 22:41:31 -0400

    logic implemented for each segment

--- src/main/java/ca/mcmaster/se2aa4/island/team09/IslandPatrol.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team09/IslandPatrol.java
@@ -7,24 +7,28 @@ public class IslandPatrol extends SearchPhase{
         super(drone, commandCenter, responseCenter, island);
     }
     @Override
-    private void executeStep(){
+    private void executeStep(){ //assume we are already in the center of the island
         //call the east north west south segments in that order with the lengths extending each time to create a square scan radiating from the center of the island
     }
-
+    
     private void eastSegment(){
-
+        drone.turnDrone(Direction.E, commandCenter);
+        drone.moveForward(commandCenter);
     }
 
     private void westSegment(){
-
+        drone.turnDrone(Direction.W, commandCenter);
+        drone.moveForward(commandCenter);
     }
 
     private void northSegment(){
-
+        drone.turnDrone(Direction.N, commandCenter);
+        drone.moveForward(commandCenter);
     }
 
     private void southSegment(){
-
+        drone.turnDrone(Direction.S, commandCenter);
+        drone.moveForward(commandCenter);
     }
     
 }
