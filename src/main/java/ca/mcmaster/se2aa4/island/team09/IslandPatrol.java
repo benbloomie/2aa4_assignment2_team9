7f514aaefb7d6e7a069013a31aa77a7d3dd9866e
cole11smith33 <cole11smith33@gmail.com>
2025-03-21 22:20:12 -0400

    Walking Skeleton for IslandPatrol

--- src/main/java/ca/mcmaster/se2aa4/island/team09/IslandPatrol.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team09/IslandPatrol.java
@@ -1,5 +1,30 @@
 package ca.mcmaster.se2aa4.island.team09;
 
-public class IslandPatrol {
+public class IslandPatrol extends SearchPhase{
+    private int segmentLength = 1;
+
+    public IslandPatrol(DroneState drone, CommandCenter commandCenter, ResponseCenter responseCenter, Island island){
+        super(drone, commandCenter, responseCenter, island);
+    }
+    @Override
+    private void executeStep(){
+        //call the east north west south segments in that order with the lengths extending each time to create a square scan radiating from the center of the island
+    }
+
+    private void eastSegment(){
+
+    }
+
+    private void westSegment(){
+
+    }
+
+    private void northSegment(){
+
+    }
+
+    private void southSegment(){
+
+    }
     
 }
