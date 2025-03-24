e53e9917a518ebf0e9b840aec4f1cfecbe308ec2
cole11smith33 <cole11smith33@gmail.com>
2025-03-24 17:42:56 -0400

    Removed unnecessary modifier code smell in Action interface

--- src/main/java/ca/mcmaster/se2aa4/island/team09/Action.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team09/Action.java
@@ -2,5 +2,5 @@ package ca.mcmaster.se2aa4.island.team09;
 
 // public interface to handle the different actions of the drone
 public interface Action {
-    public void performAction();
+    void performAction();
 }
