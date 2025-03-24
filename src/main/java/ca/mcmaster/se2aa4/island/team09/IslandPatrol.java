0a7296f15f371a51faab06bcc85ddd06697f569e
Behzad Janjua <behzadjanjua@mac.home>
2025-03-23 20:45:48 -0400

    Feat: Implementing photoscanner into search algorithm

--- src/main/java/ca/mcmaster/se2aa4/island/team09/IslandPatrol.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team09/IslandPatrol.java
@@ -4,6 +4,7 @@ public class IslandPatrol extends SearchPhase{
     private int stepCounter;
     private int moveCounter; //progress towards completion of a segment of the algorithm
     private int segmentLength = 1;
+    private boolean shouldScan = false;
     private Direction[] directions = {Direction.E, Direction.N, Direction.W, Direction.S};
 
     public IslandPatrol(DroneState drone, CommandCenter commandCenter, ResponseCenter responseCenter, Island island){
@@ -12,22 +13,33 @@ public class IslandPatrol extends SearchPhase{
     @Override
     public void executeStep(){ //assume we are already in the center of the island
         //call the east north west south segments in that order with the lengths extending each time to create a square scan radiating from the center of the island
-        int directionIndex = stepCounter%4;
-        if (moveCounter == 0){
-            drone.turnDrone(directions[directionIndex], commandCenter);
-            moveCounter++;
-        }
-        else{
-            drone.moveForward(commandCenter);
-            moveCounter++;
+        if (!shouldScan){
+            
+            int directionIndex = stepCounter%4;
+            if (moveCounter == 0){
+                drone.turnDrone(directions[directionIndex], commandCenter);
+                moveCounter++;
+            }
+            else{
+                drone.moveForward(commandCenter);
+                moveCounter++;
+            }
+            
+            if (moveCounter > segmentLength){
+                stepCounter++;
+                moveCounter = 0;
+                if (stepCounter % 2 == 0){
+                    segmentLength++;
+                }
+            }
+
+            shouldScan = true;
+
         }
         
-        if (moveCounter > segmentLength){
-            stepCounter++;
-            moveCounter = 0;
-            if (stepCounter % 2 == 0){
-                segmentLength++;
-            }
+        else {
+            drone.scan(commandCenter);
+            shouldScan = false;
         }
 
     }
