50d8673555ff41d94f47c38bf7ffdbcdf670cef9
benbloomie <bloomfib@mcmaster.ca>
2025-03-23 21:20:14 -0400

    fixing IslandLocater position location.

--- src/main/java/ca/mcmaster/se2aa4/island/team09/IslandLocater.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team09/IslandLocater.java
@@ -1,32 +1,42 @@
 package ca.mcmaster.se2aa4.island.team09;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class IslandLocater extends SearchPhase {
+    private final Logger logger = LogManager.getLogger();
     private int movesMadeToMiddle;
     private int movesMadeToIsland;
+    private int movesNeededToMiddle;
+    private int movesNeededToIsland;
     private int rangeToGround;
 
     public IslandLocater(DroneState drone, CommandCenter commandCenter, ResponseCenter responseCenter, Island island) {
         super(drone, commandCenter, responseCenter, island);
+        // calculates moves needed to island based on starting position
         this.movesMadeToMiddle = 0;
         this.movesMadeToIsland = 0;
     }
 
     @Override
     public void executeStep() {
+        if (!movesAreSet()) {
+            setMovesNeeded();
+        }
+
         // moves drone forward to middle position
-        if (movesMadeToMiddle < island.getX()/2 - 1) {
+        if (movesMadeToMiddle < movesNeededToMiddle) {
             moveToMiddle();
             return;
         }
         // turns drone when it reaches the middle of the map
-        else if (movesMadeToMiddle == island.getX()/2 - 1) {
+        else if (movesMadeToMiddle == movesNeededToMiddle) {
             faceIsland();
             return;
         }
 
-        setRangeToGround();
         // moves drone forward until it reaches GROUND 
-        if (movesMadeToIsland < rangeToGround) {
+        if (movesMadeToIsland < movesNeededToIsland) {
             moveToIsland();
             return;
         }
@@ -42,7 +52,7 @@ public class IslandLocater extends SearchPhase {
 
     private void faceIsland() {
         // updates the drone to face the island; scans to get distance to island
-        drone.turnDrone(Direction.S, commandCenter);
+        drone.turnDrone(drone.getGPS().getRightDirection(), commandCenter);
         drone.frontEcho(commandCenter);
         movesMadeToMiddle++;
     }
@@ -52,10 +62,15 @@ public class IslandLocater extends SearchPhase {
         movesMadeToIsland++;
     }
 
-    private void setRangeToGround() {
-        // now only makes one assignment to the range to GROUND
-        if (rangeToGround == 0) {
-            this.rangeToGround = responseCenter.getRadarStatus().getRange(); 
-        }
+    private boolean movesAreSet() {
+        return movesNeededToIsland != 0;
+    }
+
+    private void setMovesNeeded() {
+        // sets the amount of moves the drone needs to make to get to the islands middle position
+        this.movesNeededToMiddle = Math.abs(drone.getGPS().getXCord() - (island.getX() / 2)) - 2;
+        this.movesNeededToIsland = Math.abs(drone.getGPS().getYCord() - (island.getY() / 2)) - 2;
+        logger.info("Moves needed to middle: " + movesNeededToMiddle);
+        logger.info("Moves needed to island: " + movesNeededToIsland);
     }
 }
